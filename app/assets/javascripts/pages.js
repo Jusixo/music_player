13c89d49d4b225568dd76642291c1c7fbb5a558d
Justin Valentin <Justinvalentine88@gmail.com>
2017-08-26 16:09:05 -0400

    static base for modal to show partial page view, when each mixtape photo will be clicked it will open it's corresponding playlist partial.

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,2 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$('.playlists_showme').on('click', function(e) {
+  e.preventDefault();
+
+  $('<div id="dialog-form">Loading</div>').dialog({
+        autoOpen: true,
+        width: 520,
+        modal: true,
+        open: function() {
+            $(this).append($('#mixtapes').show());
+        },
+        close: function() {
+
+        }
+    });
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :mixtapes
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'pages/landing'
   root 'pages#landing'
