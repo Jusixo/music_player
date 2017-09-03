a0edd24debece2a1bd8680fb6d570ffa7e77c50d
Justin Valentin <Justinvalentine88@gmail.com>
2017-09-03 12:00:21 -0400

    trying to fix routing error for linking embed for playlists.

--- app/models/image_uploader.rb
+++ app/models/image_uploader.rb
@@ -11,10 +11,11 @@ class ImageUploader < Shrine
   process(:store) do |io, context|
     original = io.download
 
-    size_800 = resize_to_limit!(original, 800, 800) { |cmd| cmd.auto_orient } # orient rotated images
-    size_500 = resize_to_limit(size_800,  500, 500)
+    size_800 = resize_to_limit!(original, 561, 462) { |cmd| cmd.auto_orient } # orient rotated images
+    size_500 = resize_to_limit(size_800,  500, 250)
     size_300 = resize_to_limit(size_500,  300, 300)
     size_thumbnail = resize_to_limit(size_300, 64, 64)
+
 
     { original: io, large: size_800, medium: size_500, small: size_300, thumbnail: size_thumbnail }
   end
