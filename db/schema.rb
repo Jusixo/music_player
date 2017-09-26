9e1b504cdaa4fc847ffaa0b9cc84daa967d80631
Justin Valentin <Justinvalentine88@gmail.com>
2017-09-26 17:29:03 -0400

    added users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170914150149) do
+ActiveRecord::Schema.define(version: 20170926212436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,16 @@ ActiveRecord::Schema.define(version: 20170914150149) do
     t.datetime "updated_at", null: false
     t.text "image_data"
     t.string "mixtape_title"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "provider"
+    t.string "uid"
+    t.string "nickname"
+    t.string "access_token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
